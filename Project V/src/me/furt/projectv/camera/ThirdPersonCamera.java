da8321597f68cb8c65c607d903541b6108f1e375
furt <furt@6570a3e8-08c3-4c44-b8b8-d481236f5168>
2013-07-24 12:56:12 +0000

    camera tests

---
+++ Project V/src/me/furt/projectv/camera/ThirdPersonCamera.java
@@ -0,0 +1,11 @@
+package me.furt.projectv.camera;
+
+public class ThirdPersonCamera extends CameraTest {
+
+	public ThirdPersonCamera(float x, float y, float z, float pitch, float yaw,
+			float roll, float fov, float aspectRatio, float zNear, float zFar) {
+		super(x, y, z, pitch, yaw, roll, fov, aspectRatio, zNear, zFar);
+		// TODO Auto-generated constructor stub
+	}
+
+}
