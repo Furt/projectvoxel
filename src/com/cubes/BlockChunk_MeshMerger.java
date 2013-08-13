20afbb73bdc0a6f115137ffd6636dd24feaa1a57
Terry Earnheart <furt1337@live.com>
2013-08-13 21:36:41 +0000

    commented out physics and added flycam for testing terrain gen

--- src/com/cubes/BlockChunk_MeshMerger.java
+++
@@ -1,6 +0,0 @@
-package com.cubes;
-
-public abstract interface BlockChunk_MeshMerger {
-
-    public abstract boolean shouldFaceBeAdded(BlockChunkControl paramBlockChunkControl, Vector3Int paramVector3Int, Block.Face paramFace);
-}
--- src/com/cubes/network/BitUtil.java
+++
@@ -1,8 +0,0 @@
-package com.cubes.network;
-
-public class BitUtil {
-
-    public static int getNeededBitsCount(int value) {
-        return 32 - Integer.numberOfLeadingZeros(value);
-    }
-}
