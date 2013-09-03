65ad0816d1fbf374c80f0f7d8854cb44cdc997e0
furt <furt@6570a3e8-08c3-4c44-b8b8-d481236f5168>
2013-09-03 10:30:22 +0000

    Didnt have everything saved.

--- Cubes/src/com/cubes/network/CubesSerializer.java
+++ Cubes/src/com/cubes/network/CubesSerializer.java
@@ -10,7 +10,7 @@ public class CubesSerializer {
 
     public static byte[][][][] writeChunksToBytes(BlockTerrainControl blockTerrain) {
         BlockChunkControl[][][] chunks = blockTerrain.getChunks();
-        byte[][][] bytes = new byte[chunks.length][chunks[0].length][chunks[0][0].length];
+        byte[][][][] bytes = new byte[chunks.length][chunks[0].length][chunks[0][0].length];
         for (int x = 0; x < chunks.length; x++) {
             for (int y = 0; y < chunks[x].length; y++) {
                 for (int z = 0; z < chunks[x][y].length; z++) {
