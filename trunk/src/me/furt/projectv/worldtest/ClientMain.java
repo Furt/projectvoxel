e438a0b05231ade3c32bb699877fdf0c8e218b18
furt <furt@6570a3e8-08c3-4c44-b8b8-d481236f5168>
2013-11-03 05:23:29 +0000

    [ProjectV]added worldtest package for attempting mp worlds

---
+++ trunk/src/me/furt/projectv/worldtest/ClientMain.java
@@ -0,0 +1,82 @@
+package me.furt.projectv.worldtest;
+
+import com.cubes.BlockTerrainControl;
+import com.cubes.Vector3Int;
+import com.cubes.network.CubesSerializer;
+import com.jme3.app.SimpleApplication;
+import com.jme3.math.Vector3f;
+import com.jme3.network.Client;
+import com.jme3.network.Message;
+import com.jme3.network.MessageListener;
+import com.jme3.network.Network;
+import com.jme3.network.serializing.Serializer;
+import com.jme3.scene.Node;
+import com.jme3.system.JmeContext;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import me.furt.projectv.WorldSettings;
+
+/**
+ * ProjectV
+ *
+ * @author Furt
+ */
+public class ClientMain extends SimpleApplication {
+
+    public static void main(String[] args) {
+        ClientMain app = new ClientMain();
+        Serializer.registerClass(TerrainMessage.class);
+        app.start(JmeContext.Type.Display); // standard display type
+    }
+    public Client client;
+    public BlockTerrainControl blockTerrain;
+    public TerrainMessageListener terrainListener;
+    private ConcurrentLinkedQueue<byte[]> terrainMessageQueue;
+
+    @Override
+    public void simpleInitApp() {
+        try {
+            client = Network.connectToServer("localhost", 25570);
+            client.start();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        WorldSettings.registerBlocks();
+        blockTerrain = new BlockTerrainControl(WorldSettings.getSettings(this), new Vector3Int());
+        Node terrainNode = new Node();
+        terrainNode.addControl(blockTerrain);
+        rootNode.attachChild(terrainNode);
+        // This goes in listener
+        //CubesSerializer.readFromBytes(blockTerrainClone, msg.getWorldData());
+
+        cam.setLocation(new Vector3f(-64, 187, -55));
+        cam.lookAtDirection(new Vector3f(0.64f, -0.45f, 0.6f), Vector3f.UNIT_Y);
+        flyCam.setMoveSpeed(200);
+        
+        terrainMessageQueue = new ConcurrentLinkedQueue<byte[]>();
+        
+        terrainListener = new TerrainMessageListener();
+        client.addMessageListener(terrainListener);
+    }
+
+    public class TerrainMessageListener implements MessageListener<Client> {
+
+        public void messageReceived(Client source, Message m) {
+            if (m instanceof TerrainMessage) {
+                final TerrainMessage msg = (TerrainMessage) m;
+                terrainMessageQueue.add(msg.getWorldData());
+            }
+        }
+    }
+    
+    @Override
+    public void simpleUpdate(float tpf) {
+        byte[] message = terrainMessageQueue.poll();
+        if (message != null) {
+            CubesSerializer.readFromBytes(blockTerrain, message);
+        }
+    }
+}
