3f247318a3fbe91bba95bdb93853db75a2803dc7
furt <furt@6570a3e8-08c3-4c44-b8b8-d481236f5168>
2013-11-08 05:47:25 +0000

    [ProjectV] Added location on gui

--- trunk/src/me/furt/projectv/TestNoise.java
+++ trunk/src/me/furt/projectv/TestNoise.java
@@ -1,20 +1,31 @@
 package me.furt.projectv;
 
-import com.cubes.*;
+import com.cubes.Block;
+import com.cubes.BlockManager;
+import com.cubes.BlockNavigator;
+import com.cubes.TerrainControl;
+import com.cubes.Vector3Int;
 import com.jme3.app.SimpleApplication;
 import com.jme3.collision.CollisionResults;
 import com.jme3.font.BitmapText;
 import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.MouseButtonTrigger;
+import com.jme3.material.Material;
 import com.jme3.math.Ray;
 import com.jme3.math.Vector2f;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
 import com.jme3.system.AppSettings;
+import com.jme3.texture.Texture;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import me.furt.projectv.block.*;
 
 public class TestNoise extends SimpleApplication implements ActionListener {
@@ -22,18 +33,29 @@ public class TestNoise extends SimpleApplication implements ActionListener {
     public TerrainControl blockTerrain;
     public Node terrainNode;
     public int itemInHand;
+    public BitmapText playerLoc;
 
     public static void main(String[] args) {
         Logger.getLogger("").setLevel(Level.SEVERE);
         TestNoise app = new TestNoise();
         app.start();
     }
+    private BitmapText blockLoc;
+    private BitmapText chunkLoc;
 
     public TestNoise() {
         settings = new AppSettings(true);
         settings.setWidth(1280);
         settings.setHeight(720);
         settings.setTitle("ProjectV Demo - Noise");
+        settings.setSettingsDialogImage("/Interface/pv-splash.png");
+        try {
+            settings.setIcons(new BufferedImage[]{
+                        ImageIO.read(getClass().getResourceAsStream("/Interface/magex16.png")),
+                        ImageIO.read(getClass().getResourceAsStream("/Interface/magex32.png"))
+                    });
+        } catch (IOException e) {
+        }
     }
 
     @Override
@@ -48,6 +70,37 @@ public class TestNoise extends SimpleApplication implements ActionListener {
 
     @Override
     public void simpleUpdate(float tpf) {
+        chunkLoc.setText("Chunk   Location: X= " + cam.getLocation().getX()/4/16 + ", Y= " + cam.getLocation().getY()/4/16  + ", Z= " + cam.getLocation().getZ()/4/16);
+        blockLoc.setText("Block   Location: X= " + cam.getLocation().getX()/4 + ", Y= " + cam.getLocation().getY()/4  + ", Z= " + cam.getLocation().getZ()/4);
+        playerLoc.setText("Player  Location: X= " + cam.getLocation().getX() + ", Y= " + cam.getLocation().getY()  + ", Z= " + cam.getLocation().getZ());
+//        if (this.screen == null) {
+//            return;
+//        }
+//
+//        int percentage = Progress.getTotalPercent();
+//        String message = Progress.getLastMessage();
+//        if ((this.lastPercent != percentage) || (!ObjectUtils.areEqual(message, this.lastMessage))) {
+//            this.lastPercent = percentage;
+//            this.lastMessage = message;
+//            Label progress = (Label) this.screen.findNiftyControl("progress", Label.class);
+//
+//            if (percentage > 0) {
+//                String text = percentage + " %";
+//                progress.setText(text);
+//                if (percentage == 100) {
+//                    startGame();
+//                }
+//            } else {
+//                progress.setText("");
+//            }
+//
+//            Label status = (Label) this.screen.findNiftyControl("status", Label.class);
+//            if (message != null) {
+//                status.setText(message);
+//            } else {
+//                status.setText("");
+//            }
+//        }
     }
 
     private void initBlockTerrain() {
@@ -70,6 +123,28 @@ public class TestNoise extends SimpleApplication implements ActionListener {
         flyCam.setMoveSpeed(200);
     }
 
+    /*
+     * chunkRadius "the radius of a player that should have chunks loaded"
+     * maxChunks "the amount of chunks that can be loaded at once"
+     * seed "a predetermined long for noise"
+     * 
+     * to get chunk x z you must always round up
+     * (int)roundUp(cam/blockSize/chunkSize)
+     * 
+     */
+    public void createSelectedCube() {
+        /**
+         * An unshaded textured cube. Uses texture from jme3-test-data library!
+         */
+        Box boxshape1 = new Box(Vector3f.ZERO, 1f, 1f, 1f);
+        Geometry cube_tex = new Geometry("A Textured Box", boxshape1);
+        Material mat_tex = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        Texture tex = assetManager.loadTexture("Interface/Logo/Monkey.jpg");
+        mat_tex.setTexture("ColorMap", tex);
+        cube_tex.setMaterial(mat_tex);
+        guiNode.attachChild(cube_tex);
+    }
+    
     private void initControls() {
         inputManager.addMapping("set_block", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
         inputManager.addListener(this, "set_block");
@@ -88,6 +163,28 @@ public class TestNoise extends SimpleApplication implements ActionListener {
                 (settings.getWidth() / 2) - (guiFont.getCharSet().getRenderedSize() / 3 * 2),
                 (settings.getHeight() / 2) + (crosshair.getLineHeight() / 2), 0);
         guiNode.attachChild(crosshair);
+        
+        //Camera Location
+        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+        playerLoc = new BitmapText(guiFont, false);
+        playerLoc.setSize(guiFont.getCharSet().getRenderedSize());
+        playerLoc.setText("Player Location: X= " + cam.getLocation().getX() + ", Y= " + cam.getLocation().getY()  + ", Z= " + cam.getLocation().getZ());
+        playerLoc.setLocalTranslation(300, playerLoc.getLineHeight(), 0);
+        guiNode.attachChild(playerLoc);
+        
+        //Block Location
+        blockLoc = new BitmapText(guiFont, false);
+        blockLoc.setSize(guiFont.getCharSet().getRenderedSize());
+        blockLoc.setText("Block   Location: X= " + cam.getLocation().getX()/4 + ", Y= " + cam.getLocation().getY()/4  + ", Z= " + cam.getLocation().getZ()/4);
+        blockLoc.setLocalTranslation(300, blockLoc.getLineHeight()*2, 0);
+        guiNode.attachChild(blockLoc);
+        
+        //Chunk Location
+        chunkLoc = new BitmapText(guiFont, false);
+        chunkLoc.setSize(guiFont.getCharSet().getRenderedSize());
+        chunkLoc.setText("Chunk   Location: X= " + cam.getLocation().getX()/4/16 + ", Y= " + cam.getLocation().getY()/4/16  + ", Z= " + cam.getLocation().getZ()/4/16);
+        chunkLoc.setLocalTranslation(300, chunkLoc.getLineHeight()*3, 0);
+        guiNode.attachChild(chunkLoc);
     }
 
     @Override
@@ -95,7 +192,7 @@ public class TestNoise extends SimpleApplication implements ActionListener {
         if (action.equals("set_block") && value) {
             Vector3Int blockLocation = getCurrentPointedBlockLocation(true);
             if (blockLocation != null) {
-                Class<? extends Block> c = getBlockSelected(itemInHand);
+                Class<? extends Block> c = getBlockSelected();
                 blockTerrain.setBlock(blockLocation, c);
             }
         } else if (action.equals("remove_block") && value) {
@@ -104,7 +201,9 @@ public class TestNoise extends SimpleApplication implements ActionListener {
                 blockTerrain.removeBlock(blockLocation);
             }
         } else if (action.equals("block_select") && value) {
-            if (itemInHand >= 15) {
+
+            int temp = BlockManager.getTypes().length;
+            if (itemInHand >= temp) {
                 itemInHand = 0;
             } else {
                 itemInHand++;
@@ -129,6 +228,61 @@ public class TestNoise extends SimpleApplication implements ActionListener {
         CollisionResults results = new CollisionResults();
         node.collideWith(ray, results);
         return results;
+    }
+
+    public Class<? extends Block> getBlockSelected() {
+        switch (itemInHand) {
+            case 0:
+                return Block_Cobble.class;
+
+            case 1:
+                return Block_Dirt.class;
+
+            case 2:
+                return Block_Glass.class;
+
+            case 3:
+                return Block_Grass.class;
+
+            case 4:
+                return Block_Gravel.class;
+
+            case 5:
+                return Block_Ice.class;
+
+            case 6:
+                return Block_Lava.class;
+
+            case 7:
+                return Block_Leaves.class;
+
+            case 8:
+                return Block_Log.class;
+
+            case 9:
+                return Block_Mud.class;
+
+            case 10:
+                return Block_Plank.class;
+
+            case 11:
+                return Block_Red_Clay.class;
+
+            case 12:
+                return Block_Sand.class;
+
+            case 13:
+                return Block_Stone.class;
+
+            case 14:
+                return Block_Stone_Slab.class;
+
+            case 15:
+                return Block_Water.class;
+
+            default:
+                return Block_Grass.class;
+        }
     }
 
     // Very quick and dirty method to select a block type
---
+++ trunk/src/me/furt/projectv/states/MPGameState.java
@@ -0,0 +1,12 @@
+package me.furt.projectv.states;
+
+import com.jme3.app.state.AbstractAppState;
+
+/**
+ * ProjectV
+ *
+ * @author Furt
+ */
+public class MPGameState extends AbstractAppState {
+
+}
