62509be74c99de7cd9458ea3956bdf2f84179def
furt <furt@6570a3e8-08c3-4c44-b8b8-d481236f5168>
2013-12-23 10:33:34 +0000

    [ProjectV] Cleaned up project, part2.

--- trunk/src/me/furt/projectv/Util.java
+++ trunk/src/me/furt/projectv/Util.java
@@ -15,16 +15,12 @@ import me.furt.projectv.network.messages.*;
 class Util {
 
     static void registerSerializers() {
-        Serializer.registerClass(ActionMessage.class);
         Serializer.registerClass(ChatMessage.class);
         Serializer.registerClass(ClientJoinMessage.class);
         Serializer.registerClass(HandshakeMessage.class);
         Serializer.registerClass(LoginMessage.class);
-        Serializer.registerClass(ManualControlMessage.class);
-        Serializer.registerClass(ServerAddPlayerMessage.class);
         Serializer.registerClass(ServerJoinMessage.class);
         Serializer.registerClass(ServerTerrainMessage.class);
-        Serializer.registerClass(StartGameMessage.class);
     }
 
     static Vector3Int float2int(Vector3f vec) {
---
+++ trunk/src/me/furt/projectv/state/MPMenuState.java
@@ -0,0 +1,10 @@
+package me.furt.projectv.state;
+
+/**
+ * ProjectV
+ *
+ * @author Furt
+ */
+public class MPMenuState {
+
+}
